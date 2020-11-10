17a0c7188f41e23ef0e3f342a8be084dcface77f
duckie <sarthakmisraa@gmail.com>
2020-11-10 08:59:01 +0530

    added 2 line print in assembly

--- hello_world/hello.asm
+++ hello_world/hello.asm
@@ -4,6 +4,8 @@ section .text
 section .data
 	msg db "Hello, world", 0x0a
 	len equ $ - msg
+	msg2 db "fuck the world",0x09
+	len2 equ $ - msg2
 
 section .text
 
@@ -12,6 +14,11 @@ _start:
 	mov ebx, 1
 	mov ecx, msg
 	mov edx, len
+	int 0x80
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, msg2
+	mov edx, len2
 	int 0x80
 	mov eax,1
 	mov ebx,0
